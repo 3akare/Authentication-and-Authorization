5f933bc6d3650791174326c2da4668da24406b67
3akare <bakaredavid007@gmail.com>
2024-07-15 14:55:50 +0100

    chore: project bootstrap

---
+++ src/test/java/com/bakare/authentication_services/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakare.authentication_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
