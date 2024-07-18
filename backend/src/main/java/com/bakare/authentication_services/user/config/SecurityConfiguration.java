7d0d90b244c0876eb876af3e9ee8ae60069df6ae
3akare <bakaredavid007@gmail.com>
2024-07-18 15:29:31 +0100

    feat: security configuration

--- backend/src/main/java/com/bakare/authentication_services/user/config/SecurityConfiguration.java
+++ backend/src/main/java/com/bakare/authentication_services/user/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.bakare.authentication_services.user.config;
 
+import com.bakare.authentication_services.user.service.OAuth2LoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,21 +16,23 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+   private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private  final AuthenticationProvider authenticationProvider;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request-> request
-                        .requestMatchers("/**").permitAll()
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                       .requestMatchers("/api/v1/auth/**").permitAll()
+//                        .requestMatchers("/api/v1/video/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .oauth2Login(oauth2login ->oauth2login
-                    .loginPage("/")
-                    .defaultSuccessUrl("/api/v1/auth/token", true)
+                .oauth2Login(oauth2login -> oauth2login
+                        .loginPage("/")
+                        .successHandler(oAuth2LoginSuccessHandler)
+                        .defaultSuccessUrl("/dashboard", true)
                 );
 //                .sessionManagement(
 //                        manager->
