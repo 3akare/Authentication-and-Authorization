ed461ba0753d7356737623b1ec7907e0dcb71ee1
3akare <bakaredavid007@gmail.com>
2024-07-24 09:13:34 +0100

    test: trying to understand oauth properly

--- backend/src/main/java/com/bakare/authentication_services/user/controller/AuthenticationController.java
+++ backend/src/main/java/com/bakare/authentication_services/user/controller/AuthenticationController.java
@@ -9,10 +9,16 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,6 +31,7 @@ import java.util.Objects;
 public class AuthenticationController {
 
     private static final Logger log = LoggerFactory.getLogger(AuthenticationController.class);
+    HttpClient httpClient = HttpClient.newHttpClient();
 
     @Value("${github.client.id}")
     private String clientId;
@@ -32,109 +39,23 @@ public class AuthenticationController {
     @Value("${github.client.secret}")
     private String clientSecret;
 
-    private final RestTemplate restTemplate;
-
     @PostMapping("/github")
-    public ResponseEntity<?> githubLogin(@RequestBody GithubAuthRequest request) {
-        try {
-            String accessToken = exchangeCodeForToken(request.getCode());
-            log.info("Access token: {}", accessToken);
+    public void githubLogin(@RequestBody Map<String,String> body) throws IOException, InterruptedException {
+        Map<String,String> jsonData = new HashMap<>();
 
-            GithubUser githubUser = fetchGithubUser(accessToken);
-            log.info("GitHub user: {}", githubUser);
+        jsonData.put("client_id", "Ov23limIIiCD55RC0HXp");
+        jsonData.put("client_secret", "63cf59ae87c298b5a5974ea408569f19357ad0da");
+        jsonData.put("code", body.get("code"));
 
-//            String email = fetchGithubUserEmail(accessToken);
-//            log.info("GitHub user email: {}", email);
+        log.info("{}", jsonData);
 
-            String jwt = createJWT(githubUser, "bakaredavid007@gmail.com");
-            log.info("JWT: {}", jwt);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://github.com/login/oauth/access_token"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(jsonData.toString()))
+                .build();
 
-            return ResponseEntity.ok(jwt);
-        } catch (Exception e) {
-            log.error("GitHub login failed", e);
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("GitHub login failed");
-        }
-    }
-
-    private String exchangeCodeForToken(String code) {
-        String tokenUrl = "https://github.com/login/oauth/access_token";
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        Map<String, String> body = new HashMap<>();
-        body.put("client_id", clientId);
-        body.put("client_secret", clientSecret);
-        body.put("code", code);
-
-        HttpEntity<Map<String, String>> entity = new HttpEntity<>(body, headers);
-        ResponseEntity<String> response = restTemplate.postForEntity(tokenUrl, entity, String.class);
-        log.info("Token exchange response: {}", response.getBody());
-
-        if (response.getStatusCode() != HttpStatus.OK) {
-            log.error("Error exchanging code for token. Status: {}, Body: {}", response.getStatusCode(), response.getBody());
-            throw new RuntimeException("Failed to exchange code for token");
-        }
-
-        return parseAccessToken(Objects.requireNonNull(response.getBody()));
-    }
-
-    private GithubUser fetchGithubUser(String accessToken) {
-        String userInfoUrl = "https://api.github.com/user";
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(accessToken);
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-
-        ResponseEntity<GithubUser> response = restTemplate.exchange(userInfoUrl, HttpMethod.GET, entity, GithubUser.class);
-        if (response.getStatusCode() != HttpStatus.OK) {
-            log.error("Error fetching GitHub user. Status: {}, Body: {}", response.getStatusCode(), response.getBody());
-            throw new RuntimeException("Failed to fetch GitHub user");
-        }
-
-        log.info("GitHub user response: {}", response.getBody());
-        return response.getBody();
-    }
-
-    private String fetchGithubUserEmail(String accessToken) {
-        String userEmailUrl = "https://api.github.com/user/emails";
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(accessToken);
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-
-        ResponseEntity<String[]> response = restTemplate.exchange(userEmailUrl, HttpMethod.GET, entity, String[].class);
-        if (response.getStatusCode() != HttpStatus.OK) {
-            log.error("Error fetching GitHub user email. Status: {}, Body: {}", response.getStatusCode(), response.getBody());
-            throw new RuntimeException("Failed to fetch GitHub user email");
-        }
-
-        log.info("GitHub user email response: {}", response.getBody());
-        String[] emails = response.getBody();
-        for (String email : emails) {
-            // Assume the first email is the primary email, you may need more logic here to handle multiple emails
-            return email;
-        }
-        throw new RuntimeException("No email found for the GitHub user");
-    }
-
-    private String createJWT(GithubUser user, String email) {
-        return Jwts.builder()
-                .setSubject(user.getLogin())
-                .claim("name", user.getName())
-                .claim("email", email)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 864000000)) // 10 days
-                .signWith(SignatureAlgorithm.HS512, "YOUR_SECRET".getBytes())
-                .compact();
-    }
-
-    private String parseAccessToken(String response) {
-        String[] pairs = response.split("&");
-        for (String pair : pairs) {
-            String[] keyValue = pair.split("=");
-            if ("access_token".equals(keyValue[0])) {
-                log.info("Access Token from parse: {}", keyValue[1]);
-                return keyValue[1];
-            }
-        }
-        throw new RuntimeException("No access token found in the response");
+        HttpResponse<?> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        log.info("{}", response);
     }
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,53 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useSearchParams } from 'react-router-dom';
+import axios from 'axios';
+
 import { AuthProvider } from '@/contexts/AuthContext';
 import Login from "@/components/Login"
-import Callback from '@/components/Callback';
+// import Callback from '@/components/Callback';
 import Dashboard from '@/components/Dashboard';
 import PrivateRoute from '@/components/PrivateRoute';
+import { useEffect } from 'react';
+
+const Callback = (code) => {
+	const [callback, SetCalback] = useSearchParams();
+
+	const serverCall = async () => {
+		const response = await axios.post("http://localhost:8080/api/auth/github", {
+			code: callback.get("code")
+		})
+		console.log(response.data)
+	}
+
+	serverCall()
+	
+	return <p>{callback.get("code")}</p>
+}
 
 function App() {
 	return (
-		<AuthProvider>
-			<Router>
-				<Routes>
-					<Route path="/" element={<Login />} />
-					<Route path="/login/callback" element={<Callback />} />
-					<Route
-						path="/dashboard"
-						element={
-							<PrivateRoute>
-								<Dashboard />
-							</PrivateRoute>
-						}
-					/>
-					<Route path="*" element={<Navigate to="/" replace />} />
-				</Routes>
-			</Router>
-		</AuthProvider>
+		<Router>
+			<Routes>
+				<Route path='/' element={<a href="https://github.com/login/oauth/authorize?client_id=Ov23limIIiCD55RC0HXp&scope=user">Fuck</a>} />
+				<Route path="/callback" Component={Callback} />
+			</Routes>
+		</Router>
+		// <AuthProvider>
+		// 	<Router>
+		// 		<Routes>
+		// 			<Route path="/" element={<Login />} />
+		// 			<Route path="/login/callback" element={<Callback />} />
+		// 			<Route
+		// 				path="/dashboard"
+		// 				element={
+		// 					<PrivateRoute>
+		// 						<Dashboard />
+		// 					</PrivateRoute>
+		// 				}
+		// 			/>
+		// 			<Route path="*" element={<Navigate to="/" replace />} />
+		// 		</Routes>
+		// 	</Router>
+		// </AuthProvider>
 	);
 }
 export default App;
